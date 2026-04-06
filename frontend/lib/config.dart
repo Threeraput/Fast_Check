be20788a331bc3d54fea06df99114a9309becfa3
Patson.c <guyza182546@gmail.com>
2026-04-06 16:33:13 +0700

    flutter pub get อัพหน้าบ้านให้ version ใหม่ขึ้นมา เเต่ยังติดปัญหา report

--- frontend/lib/config.dart
+++ frontend/lib/config.dart
@@ -1,4 +1,4 @@
 class AppConfig {
-  static const String baseUrl = 'http://10.51.151.125:8000/api/v1';
-  static const String uploadsfileUrl = 'http://10.51.151.125:8000';
+  static const String baseUrl = 'http://192.168.1.42:8000/api/v1';
+  static const String uploadsfileUrl = 'http://192.168.1.42:8000';
 }
